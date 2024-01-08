35399ce313c6c6cd72c7b47d2589c86bd32c0ce5
naserrasoulii <nrasoli3@gmail.com>
2024-01-08 15:07:41 +0330

    some edit in notepad

--- src/Redux/Reducer/Todos.js
+++ src/Redux/Reducer/Todos.js
@@ -1,9 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 console.log(createAsyncThunk);
-const initialState = { value: 0 };
+const initialState = { value: 00 };
 const counterSlice = createSlice({
   name: "counter",
   initialState,
+
   reducers: {
     addTodo(state) {
       state.value++;
